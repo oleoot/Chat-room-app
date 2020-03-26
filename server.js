7b86eb641c17d860e8bb1e3a40b4e141b14cf394
oleoot <oleoot28@gmail.com>
2020-03-26 12:55:44 +0200

    actions when user joins and leaves the chat

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+
 
 const app = express();
-
+const server = http.createServer(app);
+const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// run when client connects
+io.on("connection", (socket) => {
+    // Welcome current user
+    socket.emit("message", "Welcome to Chat-room!");
+    // when user connects
+    socket.broadcast.emit("message", "A user has joined the chat");
+    // when client dissconnertcs
+    socket.on("disconnect", () => {
+        io.emmit("message", "A user has left the chat")
+    })
+})
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
